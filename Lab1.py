e28f033e63f24e07a453b4dde2c65de68c638261
gitbyrne19 <gitbyrne19@gmail.com>
2024-10-15 19:29:17 -0700

    Add greet function

--- Lab1.py
+++ Lab1.py
@@ -5,3 +5,9 @@ print("Hello git!")
 print("Hello world")
 print("Learning about git diff. ")
 print(f"Today is {datetime.date.today()}")
+
+
+def greet(name):
+    return f"Hello, {name}! Welcome to Git and Github."
+
+print(greet("Luke"))

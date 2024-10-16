4d69a01f4b10a25dd24552e58033de7cb57d66b5
gitbyrne19 <gitbyrne19@gmail.com>
2024-10-15 19:44:33 -0700

    Resolve merge conflict

--- Lab1.py
+++ Lab1.py
@@ -14,9 +14,6 @@ print(greet("Luke"))
 
 print("This line was added directly on GitHub.")
 
-<<<<<<< HEAD
 print("This line was added locally.")
-=======
 
 print("Another line added on GitHub.")
->>>>>>> 95692e3411bc645b9759c857983409040074c80f

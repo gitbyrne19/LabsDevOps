415190a0761c75db6e67a05497c2841b44ea1b58
gitbyrne19 <gitbyrne19@gmail.com>
2024-10-15 21:47:56 -0700

    Add feature A

--- Lab1.py
+++ Lab1.py
@@ -14,12 +14,8 @@ print(greet("Luke"))
 
 print("This line was added directly on GitHub.")
 
-print("This line was added locally.")
+print("This line was added locally.")   
 
-print("Another line added on GitHub.")
-
+    
 def feature_a():
     return "This is feature A"
-
-def feature_b():
-    return "This is feature B"

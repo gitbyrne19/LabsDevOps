32646bb3a92799b333d25dbd36550ec824b02ce8
gitbyrne19 <gitbyrne19@gmail.com>
2024-10-15 19:59:44 -0700

    Add divide function

--- calculator.py
+++ calculator.py
@@ -5,4 +5,10 @@ def subtract(a, b):
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Division by zero"
+
 print(multiply(5,6))
